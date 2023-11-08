a327ee1bbffffd1d858f5d76d8d8e7aca37b4302
hagenzhang <hagenzhang2015@gmail.com>
2023-11-08 13:42:00 -0500

    tiny cleanup

--- townService/src/town/games/ChessGame.ts
+++ townService/src/town/games/ChessGame.ts
@@ -33,7 +33,7 @@ export default class ChessGame extends Game<ChessGameState, ChessMove> {
   }
 
   private get _board() {
-    return this._chessBoard; // TODO
+    return this._chessBoard;
   }
 
   private _checkForGameEnding() {
@@ -75,19 +75,7 @@ export default class ChessGame extends Game<ChessGameState, ChessMove> {
    * Uses the player's ID to determine which game piece they are using (ignores move.gamePiece)
    * Validates the move before applying it. If the move is invalid, throws an InvalidParametersError with
    * the error message specified below.
-   * A move is invalid if:
-   *    - The move is out of bounds (not in the 3x3 grid - use MOVE_OUT_OF_BOUNDS_MESSAGE)
-   *    - The move is on a space that is already occupied (use BOARD_POSITION_NOT_EMPTY_MESSAGE)
-   *    - The move is not the player's turn (MOVE_NOT_YOUR_TURN_MESSAGE)
-   *    - The game is not in progress (GAME_NOT_IN_PROGRESS_MESSAGE)
-   *
-   * If the move is valid, applies the move to the game and updates the game state.
-   *
-   * If the move ends the game, updates the game's state.
-   * If the move results in a tie, updates the game's state to set the status to OVER and sets winner to undefined.
-   * If the move results in a win, updates the game's state to set the status to OVER and sets the winner to the player who made the move.
-   * A player wins if they have 3 in a row (horizontally, vertically, or diagonally).
-   *
+   * 
    * @param move The move to apply to the game
    * @throws InvalidParametersError if the move is invalid
    */
