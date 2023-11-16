62f8abe5b7a1f0c0108642656196463838b6a80f
Owills <oliver.w.marker@gmail.com>
2023-11-15 21:54:00 -0500

    board rotation & en passant

--- frontend/src/classes/interactable/ChessAreaController.ts
+++ frontend/src/classes/interactable/ChessAreaController.ts
@@ -50,15 +50,15 @@ export default class ChessAreaController extends GameAreaController<ChessGameSta
   */
   protected init_board(): ChessCell[][] {
     return  [
-      [undefined,undefined,undefined,new Queen("B",7,3),new King("B",7,4),undefined,undefined,undefined],
-      [new Pawn("B",7,0),new Pawn("B",7,1),new Pawn("B",7,2),new Pawn("B",7,3),new Pawn("B",7,4),new Pawn("B",7,5),new Pawn("B",7,6),new Pawn("B",7,7)],
-      [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-      [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-      [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-      [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
-      [new Pawn("W",1,0),new Pawn("W",1,1),new Pawn("W",1,2),new Pawn("W",1,3),new Pawn("W",1,4),new Pawn("W",1,5),new Pawn("W",1,6),new Pawn("W",1,7)],
-      [undefined,undefined,undefined,new Queen("W",0,3), new King("W",0,4),undefined,undefined,undefined],
-    ];
+      [undefined,undefined,undefined,new Queen("W",0,3),new King("W",0,4),undefined,undefined,undefined],
+        [new Pawn("W",1,0),new Pawn("W",1,1),new Pawn("W",2,3),new Pawn("W",1,3),new Pawn("W",1,4),new Pawn("W",1,5),new Pawn("W",1,6),new Pawn("W",1,7)],
+        [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
+        [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
+        [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
+        [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
+        [new Pawn("B",6,0),new Pawn("B",6,1),new Pawn("B",6,2),new Pawn("B",6,3),new Pawn("B",6,4),new Pawn("B",6,5),new Pawn("B",6,6),new Pawn("B",6,7)],
+        [undefined,undefined,undefined,new Queen("B",7,3),new King("B",7,4),undefined,undefined,undefined],
+      ];
   }
 
   /**
