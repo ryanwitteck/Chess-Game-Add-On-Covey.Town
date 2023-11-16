1e62a3292f18dc8d29a62a860b1e2f964a703716
achen32 <aretha.chen@gmail.com>
2023-11-16 02:05:08 -0500

    rook and bishop

--- townService/src/town/games/Chess/ChessPieces/Bishop.ts
+++ townService/src/town/games/Chess/ChessPieces/Bishop.ts
@@ -9,7 +9,7 @@ import {
   IChessPiece,
 } from '../../../../types/CoveyTownSocket';
 
-export default class King implements IChessPiece {
+export default class Bishop implements IChessPiece {
   color: ChessColor;
 
   row: ChessSquare;
@@ -25,11 +25,51 @@ export default class King implements IChessPiece {
     this.type = 'B';
   }
 
+  
   validate_move(
     newRow: ChessSquare,
     newCol: ChessSquare,
     board: ChessCell[][],
     moves: ReadonlyArray<ChessMove>,
   ) {
+    const rowDiff = Math.abs(newRow - this.row);
+    const colDiff = Math.abs(newCol - this.col);
+
+    // check if the move is diagonal
+    if (rowDiff === colDiff && rowDiff > 0) {
+      // check if there are any pieces in the path
+      if (!this._isPathClear(newRow, newCol, board)) {
+        throw new InvalidParametersError('Failed path check in Bishop');
+      }
+
+      // check if the destination square is empty or has an opponent's piece
+      const destinationPiece = board[newRow][newCol];
+      if (!(destinationPiece === null || destinationPiece?.color !== this.color)) {
+        throw new InvalidParametersError('Failed dest check in Bishop');
+      }
+    } else {
+      throw new InvalidParametersError(INVALID_MOVE_MESSAGE);
+    }
+  }
+
+  private _isPathClear(newRow: ChessSquare, newCol: ChessSquare, board: ChessCell[][]) {
+    const rowIncrement = newRow > this.row ? 1 : -1;
+    const colIncrement = newCol > this.col ? 1 : -1;
+
+    let currentRow = this.row + rowIncrement;
+    let currentCol = this.col + colIncrement;
+
+    while (currentRow !== newRow || currentCol !== newCol) {
+      if (board[currentRow][currentCol] !== undefined) {
+        return false;
+      }
+
+      currentRow += rowIncrement;
+      currentCol += colIncrement;
+    }
+
+    return true;
   }
 }
+
+
--- townService/src/town/games/Chess/ChessPieces/Rook.ts
+++ townService/src/town/games/Chess/ChessPieces/Rook.ts
@@ -9,7 +9,7 @@ import {
   IChessPiece,
 } from '../../../../types/CoveyTownSocket';
 
-export default class King implements IChessPiece {
+export default class Rook implements IChessPiece {
   color: ChessColor;
 
   row: ChessSquare;
@@ -31,5 +31,42 @@ export default class King implements IChessPiece {
     board: ChessCell[][],
     moves: ReadonlyArray<ChessMove>,
   ) {
+    const rowDiff = Math.abs(newRow - this.row);
+    const colDiff = Math.abs(newCol - this.col);
+
+    // check if the move is horizontal or vertical
+    if ((rowDiff === 0 && colDiff > 0) || (rowDiff > 0 && colDiff === 0)) {
+      // check if there are any pieces in the path
+      if (!this._isPathClear(newRow, newCol, board)) {
+        throw new InvalidParametersError('Failed path check in Rook');
+      }
+
+      // check if the destination square is empty or has an opponent's piece
+      const destinationPiece = board[newRow][newCol];
+      if (!(destinationPiece === null || destinationPiece?.color !== this.color)) {
+        throw new InvalidParametersError('Failed dest check in Rook');
+      }
+    } else {
+      throw new InvalidParametersError(INVALID_MOVE_MESSAGE);
+    }
+  }
+
+  private _isPathClear(newRow: ChessSquare, newCol: ChessSquare, board: ChessCell[][]) {
+    const rowIncrement = newRow > this.row ? 1 : newRow < this.row ? -1 : 0;
+    const colIncrement = newCol > this.col ? 1 : newCol < this.col ? -1 : 0;
+
+    let currentRow = this.row + rowIncrement;
+    let currentCol = this.col + colIncrement;
+
+    while (currentRow !== newRow || currentCol !== newCol) {
+      if (board[currentRow][currentCol] !== undefined) {
+        return false;
+      }
+
+      currentRow += rowIncrement;
+      currentCol += colIncrement;
+    }
+
+    return true;
   }
 }
